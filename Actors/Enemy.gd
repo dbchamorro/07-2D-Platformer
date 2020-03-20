dbdb29430d5b8ed247988e82b6d6f7043e529f5a
Destiny C <dbchamorro@yahoo.com>
2020-03-20 09:54:28 -0400

    Added Portal

--- Actors/Enemy.gd
+++ Actors/Enemy.gd
@@ -21,11 +21,12 @@ func _on_StompDetector_body_entered(body:PhysicsBody2D) -> void:
 	queue_free()
 
 func _physics_process(delta: float) -> void:
+	_velocity.y += gravity * delta
 	if is_on_wall():
 		_velocity.x *= -1.0
 	else:
 		1
-	_velocity = move_and_slide(_velocity, FLOOR_NORMAL)
+	_velocity.y = move_and_slide(_velocity, FLOOR_NORMAL).y
 
 func die() -> void:
 	PlayerData.score += score
