de60e5721d7113cb7352ac10150c2280af3f8273
Destiny C <dbchamorro@yahoo.com>
2020-03-20 08:48:06 -0400

    Update Coin.gd

--- src/Objects/Coin.gd
+++ src/Objects/Coin.gd
@@ -1,8 +1,10 @@
 extends Area2D
 
+export var score = 100
 onready var anim_player: AnimationPlayer = get_node("AnimationPlayer")
 
 
 func _on_Coin_body_entered(body: PhysicsBody2D) -> void:
 	anim_player.play("fade_out")
+	PlayerData.score += score
 
