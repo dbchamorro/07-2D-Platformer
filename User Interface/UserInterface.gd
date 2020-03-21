4a4b95ceeee9a9565fca6c0d9e984d319e7a6414
Destiny C <dbchamorro@yahoo.com>
2020-03-20 23:24:59 -0400

    ERROR FOUND
    After player dies pause menu shows, but buttons on the pause menu do not work.

--- User Interface/UserInterface.gd
+++ User Interface/UserInterface.gd
@@ -1,9 +1,13 @@
 extends Control
 
 onready var scene_tree: = get_tree()
+#onready var score_label: Lable = $Score
 onready var pause_overlay: ColorRect = get_node("PauseOverlay")
 onready var score: Label = get_node("Label")
-onready var pause_title: Label = get_node("PauseOverlay/Title")
+onready var title_label: Label = get_node("PauseOverlay/Title")
+onready var main_screen_button: Button = $PauseOverlay/VBoxContainer/ChangeSceneButton
+
+const MESSAGE_DIED = "You died"
 
 var paused: = false setget set_paused
 
@@ -14,12 +18,15 @@ func _ready():
 
 func _on_PlayerData_player_died() -> void:
 	self.paused = true
-	pause_title.text = "You died!"
+	title_label.text = MESSAGE_DIED
+
+func _on_Player_reset() -> void:
+	self.paused = false
 
 func _unhandled_input(event: InputEvent) -> void:
-	if event.is_action_pressed("pause"):
+	if event.is_action_pressed("pause") and title_label.text != MESSAGE_DIED:
 		self.paused =  not paused
-		scene_tree.set_input_as_handled()
+		#scene_tree.set_input_as_handled()
 
 func update_interface() -> void:
 	score.text = "Score %s" % PlayerData.score
